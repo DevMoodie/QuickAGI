332389f6613b6bc512365fd97082feadb5089b39
DevMoodie <Mahmoudyf96@gmail.com>
2023-04-14 09:56:42 -0400

    Initial Commit

---
+++ QuickAGI/QuickAGIApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuickAGIApp.swift
+//  QuickAGI
+//
+//  Created by Moody on 2023-04-14.
+//
+
+import SwiftUI
+
+@main
+struct QuickAGIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
